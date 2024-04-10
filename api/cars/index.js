3295f29176215d01fc273f17e50c1cc66f563ead
teekayseboni <tholwana@icloud.com>
2024-04-10 21:51:57 +0200

    cars.json file change

--- api/cars/index.js
+++ api/cars/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-    const cars = require('../../src/cars.json')
+    const cars = require('./cars.json')
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: cars
